55f0085da0e9162f2e61262810520fa50b9f4d4b
ErenbolukGappze <erenboluk@hotmail.com>
2023-06-27 12:52:37 +0300

    Added MapView and adjusted location settings

--- Travel Book/Travel Book/ViewController.swift
+++ Travel Book/Travel Book/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()	
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let coords = locations[0].coordinate
+        let location = CLLocationCoordinate2D(latitude: coords.latitude , longitude: coords.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        mapView.setRegion(region, animated: true)
     }
 
 
